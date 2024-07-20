2217641b4a334f8ccb74a8a15b87e02d22111b31
Abdullah Zulfiqar <164308085+AbdullahZulfiqar2005@users.noreply.github.com>
2024-07-20 21:56:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const boardElement = document.getElementById("chessboard");
+
+    let board = [
+        ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+        ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']
+    ];
+
+    const pieces = {
+        'r': 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg',
+        'n': 'https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg',
+        'b': 'https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg',
+        'q': 'https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg',
+        'k': 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg',
+        'p': 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg',
+        'R': 'https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg',
+        'N': 'https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg',
+        'B': 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg',
+        'Q': 'https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg',
+        'K': 'https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg',
+        'P': 'https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg'
+    };
+
+  
+    function drawBoard() {
+        boardElement.innerHTML = ''; 
+
+        board.forEach((row, rowIndex) => {
+            row.forEach((piece, colIndex) => {
+                createSquare(piece, rowIndex, colIndex);
+            });
+        });
+    }
+
+    function createSquare(piece, rowIndex, colIndex) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+
+        if ((rowIndex + colIndex) % 2 === 0) {
+            square.classList.add("white");
+        } else {
+            square.classList.add("green");
+        }
+
+        if (piece !== ' ') {
+            const img = document.createElement("img");
+            img.src = pieces[piece];
+            square.appendChild(img);
+        }
+
+        boardElement.appendChild(square);
+    }
+
+    drawBoard(); 
+});
